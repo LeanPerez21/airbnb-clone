c76b5feb7f0e51b71069a14b2ded6f23eac32a1c
Lean21 <leandroperezz1998@gmail.com>
2020-08-06 21:46:06 -0300

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+// Boton flotante en footer
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Obtener la imagen
+
+    const imagenHero = document.querySelector(".hero");
+
+    // Inicializar las variables
+    let i = 0;
+    let tiempo = 0;
+
+    // Arreglo con imagenes de fondo
+    const imagenes = ["arriba2.jpg","arriba.jpg"];
+
+    setInterval( () => {
+        imagenHero.style.backgroundPositionY = "-"+tiempo+"px";
+
+
+        if(tiempo >40) {
+
+            tiempo = 0;
+
+            imagenHero.style.backgroundImage = "url(../img/"+imagenes[i]+")";
+
+            if(i === imagenes.length -1 ) {
+                i = 0;
+            } else {
+                i++;
+            }
+            console.log(i);
+
+        } else {
+
+            
+
+        }
+        tiempo += 1;
+        
+        //console.log(tiempo);
+    },100); 
+
+    const btnFlotante = document.querySelector('.btn-flotante');
+    btnFlotante.addEventListener("click", (e) =>{
+        e.preventDefault();
+
+
+
+        // Prevenimos default, pero ejecutamos el siguiente codigo
+
+        const footer = document.querySelector("#footer");
+
+        if(footer.classList.contains("activo")){
+            //Si lo contiene, ejecuta esto
+            footer.classList.remove("activo");
+            btnFlotante.classList.remove("activo");
+            btnFlotante.innerText = "Idioma y Moneda";
+            
+        } else {
+            // Se agrega la clase activo
+            footer.classList.add("activo");
+            btnFlotante.classList.add("activo");
+            btnFlotante.innerText = "X Cerrar";
+        }
+    });
+
+
+} );
+
+
+
